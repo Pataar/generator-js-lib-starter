74d052358840a199f89dd1cf93956a414845e601
Pataar <pietering1@gmail.com>
2015-06-02 20:58:00 +0200

    Did not call constructor of the library

--- generators/app/templates/src/_main.js
+++ generators/app/templates/src/_main.js
@@ -3,15 +3,17 @@
     version <%= version %>
     by <%=author%>
 */
-var <%=projectname%> = function (root) {    
-    
+var <%=projectname%> = function (root) {
+    this.root = root;
     this.version = "<%= version %>";
+
+	//your magic
     
     return this;        
 };
 
 (function (root) {
     
-    root.<%=projectname%> = <%=projectname%>;
+    root.<%=projectname%> = <%=projectname%>(root);
     
-})(this);
+})(window);
